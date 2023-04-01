ed87a57e6c465ffc37c95bc23707ff9723e25c98
JDanielJ <jose.granados.murillo@est.una.ac.cr>
2023-03-31 18:08:03 -0600

    Fixed align images

--- src/root/components/Container/Container.tsx
+++ src/root/components/Container/Container.tsx
@@ -1,9 +1,6 @@
-import React, { useState, useEffect, useCallback, useMemo } from 'react'
-import Image from 'next/image'
+import React, { useState, useCallback } from 'react'
 import ImgContainer from '../ImgContainer/ImgContainer'
 import InfoHeader from '../InfoHeader/InfoHeader'
-
-
 
 interface Information {
    id: string
@@ -11,6 +8,7 @@ interface Information {
 }
 
 const Container = () => {
+   
    const [images, setImages] = useState<Information[]>([])
    const [page, setPage] = useState(1)
 
@@ -18,26 +16,29 @@ const Container = () => {
 
    const getImagesInfo=useCallback(()=>{
       fetch(`https://api.unsplash.com/photos/?client_id=DtRkN9Uc2c5u33Ehd8ehhblvbES-PAHThWw40uhYHcs&page=${page} `)
+
          .then((response) => response.json())
-         .then((data) => {
-            setImages([...images, ...data])
-         })
+         .then((data) => {setImages([...images, ...data])})
          .catch((error) => console.error(error))
+
    },[page])();
 
    return (
       <main>
+
          <InfoHeader />
          <div className=" grid grid-cols-5 justify-items-center mt-4 order-last md:grid-cols-3 xl:grid-cols-4 sm:grid-cols-2 xs:grid-cols-1 pb-12">
+
             {images.map((imag, index) => (
                <ImgContainer key={index} srcInfo={imag.urls.small} altInfo="" />
             ))}
+
          </div>
+
          <div className=" w-full px-auto b bg-slate-500 flex  justify-center fixed bottom-0">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-5 m-4 rounded-full" onClick={loadImagesClick}>
-               Add more images
-            </button>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-5 m-4 rounded-full" onClick={loadImagesClick}> Add more images </button>
          </div>
+
       </main>
    )
 }
--- src/root/components/ImgContainer/ImgContainer.tsx
+++ src/root/components/ImgContainer/ImgContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react'
+import React, {useState } from 'react'
 import Image from 'next/image'
 
 interface SrcImage {
@@ -16,9 +16,9 @@ const ImgContainer = ({ srcInfo, altInfo }: SrcImage) => {
       };
 
       hideElement()
-      
+
       return (
-         <div className='w-full h-full relative'>
+         <div className='w-full h-full relative flex justify-center'>
             {isHide?null:<p className=' absolute -top-4 animate-ren text-base font-mono' >Render...</p>}
              <Image className=" h-4/5 w-4/5" src={srcInfo} alt={altInfo} width={500} height={500} unoptimized/>
          </div>
@@ -26,4 +26,4 @@ const ImgContainer = ({ srcInfo, altInfo }: SrcImage) => {
       )
 }
 
-export default React.memo(ImgContainer)//Cambiar por el useMemo o explicar bien esta
+export default React.memo(ImgContainer)
